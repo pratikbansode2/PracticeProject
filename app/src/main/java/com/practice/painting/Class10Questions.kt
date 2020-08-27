4aa20cc24a02be38bfd61abc0a89e865fa369b2e
S___nil <spwankhede007@gmail.com>
2020-08-27 22:40:36 +0530

    Added new activities and intent

---
+++ app/src/main/java/com/practice/painting/Class10Questions.kt
@@ -0,0 +1,11 @@
+package com.practice.painting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Class10Questions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class10_questions)
+    }
+}
---
+++ app/src/main/java/com/practice/painting/Class12Questions.kt
@@ -0,0 +1,11 @@
+package com.practice.painting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Class12Questions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class12_questions)
+    }
+}
